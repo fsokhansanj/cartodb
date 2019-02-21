6688dd22dfc3bbc3f616d3aa53f449679e04f1f9
Farz Sokhansanj <fsokhansanj@gmail.com>
2019-02-20 16:21:00 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {
-  nbApiToken: 'a54a82f5c1c28b3936db92dca35f8382e509c8944156c53b4bcdd14d49691295',
-  nbSlug: 'chriswhong',
+  nbApiToken: '',
+  nbSlug: '',
   nbRows: 100,
-  cdbUsername: 'fsokhansanj',
-  cdbApiKey: '086023c1ec7a0137aef3d17051e638c0a32e1679',
-  cdbTablename: 'farz_test'
+  cdbUsername: '',
+  cdbApiKey: '',
+  cdbTablename: ''
 }
 
 
